7820774fb4e701ebd10f96ad9d04e35f6b72cfcf
sergejkosilov <info@addeo.ru>
2025-10-08 20:15:27 +0300

    Update environment production URLs and submodule reference; change API and authentication endpoints to new ngrok URLs and update .tmp-deploy to point to the latest subproject commit.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://0e0b16cb226b.ngrok-free.app/api',
-  authUrl: 'https://0e0b16cb226b.ngrok-free.app/api/auth/login',
+  apiUrl: 'https://1e53debf9f5f.ngrok-free.app/api',
+  authUrl: 'https://1e53debf9f5f.ngrok-free.app/api/auth/login',
   appName: 'Coffee Admin Panel Demo',
   demo: true,
 };
