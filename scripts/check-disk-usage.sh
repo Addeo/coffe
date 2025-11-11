076465a37e62967111f52051f0257cff5212ab11
sergejkosilov <info@addeo.ru>
2025-11-11 12:50:52 +0300

    8 version

--- scripts/check-disk-usage.sh
+++ scripts/check-disk-usage.sh
@@ -9,7 +9,8 @@ set -e
 VPS_HOST="192.144.12.102"
 VPS_USER="user1"
 PROJECT_DIR="~/coffe"
-SSH_OPTS="-o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ConnectTimeout=30"
+SSH_KEY="${SSH_KEY:-$HOME/.ssh/coffe_key}"
+SSH_OPTS="-i ${SSH_KEY} -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ConnectTimeout=30"
 
 ssh_exec() {
   ssh ${SSH_OPTS} "${VPS_USER}@${VPS_HOST}" "$@"
